7661429d54959dec22f802b3f7088af9c3cbaae3
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-06-10 12:49:09 -0300

    style: página inicial

--- src/utils/vars.ts
+++ src/utils/vars.ts
@@ -1,2 +1,2 @@
-export const PACOCA_API_URL = import.meta.env.VITE_PACOCA_API_URL ?? "http://127.0.0.1:8081/api";
-export const RITA_API_URL = import.meta.env.VITE_RITA_API_URL ?? "http://127.0.0.1:8085/api";
+export const PACOCA_API_URL = import.meta.env.VITE_PACOCA_API_URL ?? "https://rita-back.pacoca.site/api";
+export const RITA_API_URL = import.meta.env.VITE_RITA_API_URL ?? "https://pacoca.net/api";
