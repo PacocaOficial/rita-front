fab9c5f381e99ae3e930145cddd8a3c58d3e6a8a
JoaoEnrique <joaoenriquealves13@gmail.com>
2025-06-11 13:00:19 -0300

    teste

--- src/utils/vars.ts
+++ src/utils/vars.ts
@@ -1,2 +1,2 @@
-export const RITA_API_URL = import.meta.env.VITE_RITA_API_URL ?? "http://localhost:8085/api";
-export const PACOCA_API_URL = import.meta.env.VITE_PACOCA_API_URL ?? RITA_API_URL;
+export const RITA_API_URL = import.meta.env.VITE_RITA_API_URL ?? "https://rita-back.pacoca.site/api";
+export const PACOCA_API_URL = import.meta.env.VITE_PACOCA_API_URL ?? "https://laravel.pacoca.net/api";
